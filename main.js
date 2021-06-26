b34605c9f95ef81336fab8f983e334a4edb1c331
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-06-26 20:21:12 -0300

    cuarto commit

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 //Defino funciones:
 const Saludar =(nombre) => alert(`¡Hola ${nombre}! En este apartado veremos alguna funciones basicas de física`)
 //calcula velocidad en mt/min:
-const velMetMin = (distancia, tiempo) => (distancia/tiempo).toFixed(2) + ' metros/minuto';
+function velMetMin(distancia,tiempo) {
+  if (tiempo == 0) {
+      return 'Ocurrió un error!'
+  }else if(tiempo != 0){
+    return (distancia/tiempo).toFixed(2) + ' metros/minuto';
+  }         
+}
 //En la siguiente funcion calculo los metros que hago en 60 minutos, y luego los paso como Km/h
 function pasarKmxHora(distancia,tiempo) {
-    let calcudist = (((60 * distancia) / tiempo) * 0.01).toFixed(2);
-    return `${calcudist} Km/hora`
+    if(tiempo == 0){
+        return ('Eso  no es calculable lo siento')
+    }else if(tiempo !=0){
+        let calcudist = (((60 * distancia) / tiempo) * 0.01).toFixed(2);
+        return `${calcudist} Km/hora`
+    }
 }
 //calculador de aceleracion:
 //Aclaracion, dividi por 3600 porque tenía que hacer una conversion de horas a egundos, respetando las unidades de la aceleracion
@@ -15,10 +25,8 @@ function aceleracion(v1, v2, v3, v4) {
     let vel2 = v2/3600
     let vel3 = v3/3600
     let vel4 = v4/3600
-    return 'Tienes una aceleración de ' ((vel1 + vel2 + vel3 + vel4 ) / 4).toFixed(2) + ' metros/seg2'
+    return 'Tienes una aceleración de ' +((vel1 + vel2 + vel3 + vel4 ) / 4).toFixed(2) + ' metros/seg2'
 }
-
-
 
 
 //Aplico funciones:
@@ -31,8 +39,8 @@ let distancia1= Number(prompt('Dime distancia en metros'))
 let tiempo1= Number(prompt('Dime tiempo en minutos'))
 
 console.log(velMetMin(distancia1,tiempo1));
-console.log(`lo mismo que ` + pasarKmxHora(distancia1, tiempo1));
-//AGREGAR WHILE! a = Δv/t
+console.log(pasarKmxHora(distancia1, tiempo1));
+
 alert(`Ahora podemos ver la aceleración(a), que básicamente podría verse como un promedio de velocidad en X tiempo. A continuación calcularemos a. Toma como ejemplo una situacion en la cual sales de paseo en auto.`)
 
 let v1 = Number(prompt('Arrancas el auto, en este momento que velocidad tienes? (coloca solo el numero, tomamos como unidad los Km/hora)'))
