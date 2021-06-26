a8deda64056a0dcfbe58532bd92d4db066b0d83c
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-06-26 13:16:45 -0300

    seundo commit

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 //TRABAJO COMPLEMENTARIO, FUNCIONES!
-
-function saluda(nombre){
-    alert(`¡Hola ${nombre}! En este apartado veremos alguna funciones basicas de física, será genial!`)
+//Defino funciones:
+const Saludar =(nombre) => alert(`¡Hola ${nombre}! En este apartado veremos alguna funciones basicas de física`)
+//calcula velocidad en mt/min:
+const velMetMin = (distancia, tiempo) => (distancia/tiempo).toFixed(2) + ' metros/minuto';
+//En la siguiente funcion calculo los metros que hago en 60 minutos, y luego los paso como Km/h
+function pasarKmxHora(distancia,tiempo) {
+    let calcudist = ((60 * distancia) / tiempo) * 0.01;
+    return `${calcudist} Km/hora`
 }
 
+//Aplico funciones:
 let nombre =prompt('Bienvenido,¿como te llamas?')
-saluda(nombre)
+Saludar(nombre)
 
 alert('Vamos a ver la velocidad! Es una medida de tiempo sobre espacio, a continuación tendrás un cálculador de velocidad')
-
-
-/*function velocidad(distancia,tiempo){
-    let vel = (distancia / tiempo)
-    return vel; 
-}
-*/
-const velMetMin = (distancia, tiempo) => (distancia/tiempo).toFixed(2) + ' metros/minuto';
-const velKmHor = (distancia, tiempo) => (distancia/tiempo).toFixed(2) + ' Kilometros/hora';
-const velMetHor = (distancia, tiempo) => (distancia/tiempo).toFixed(2) + ' metros/hora';
 
 let distancia1= Number(prompt('Dime distancia en metros'))
 let tiempo1= Number(prompt('Dime tiempo en minutos'))
 
 console.log(velMetMin(distancia1,tiempo1));
-
-
- 
-
+console.log(`lo mismo que ` + pasarKmxHora(distancia1, tiempo1));
 
 
 
