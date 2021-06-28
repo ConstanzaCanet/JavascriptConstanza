9752d198d1d5a8a7a4f5f2099bca8b687a9e5e68
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-06-28 20:37:01 -0300

    primero

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 //PRACTICANDO OBJETOS
+class Vehicle{
+  constructor(color, year, trademark, price){
+        this.color=color;
+        this.year=year;
+        this.trademark=trademark;
+        this.price=price;
+  }
+  iva(){
+    let result = this.price * 0.21
+    return result
+  }
+  priceIva(){
+    let total = this.price + this.iva()
+    return total;
+  }
+  
+
+}
+
+//Objeto de ejemplo
+const CarOne= new Vehicle ('negro', '2005', 'BMW', 56.400 );
+
+console.log(CarOne)
+console.log(CarOne.priceIva())
