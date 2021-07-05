600de1fbe842107d34f6b8e17f63e522a790c752
ConstanzaCarnet <carnetconstanza@gmail.com>
2021-07-05 17:05:05 -0300

    tercer commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+//PARA LA ENTREGA DEL PRIMER PROYECTO FINAL, CONTINUARE TRABAJNDO CON LAS IDEAS DE LOS PROYECTOS ANTERIORES
+// DE MODO QUE PUEDO PRESENTAR FUNCIONES O METODOS JUNTO CON SUS RESPECTIVOS OBJETOS, LA IDEA ES HACER UN CATALOGO DE PELICULAS.
+
+//defino objeto pelicula:
+
 class Movie{
   constructor(title ,gender, duration, year, fullprice){
     this.title = title;
@@ -8,6 +13,8 @@ class Movie{
   }
 }
 
+
+//defino objeto usuario o persona:
 class Person{
   constructor(namePerson, lastName, age, nationality, email, cart){
       this.namePerson = namePerson;
@@ -17,9 +24,31 @@ class Person{
       this.email = email;
       this.cart = cart;
   }
-  //ESTA FUNCIÓN ESTABA DE ANTES, LA COLOCO AHORA EN OBJETO PERSONA
-  //SOLO PARA REGISTRADOS ESTARA DISPONIBLE LA FUNCION COMPRAR- 
-  //REALIZO UNOS CAMBIOS EN LA FUNCION(SIMULO CARRITO)
+
+  showprice(){
+    let result= this.fullprice * 0.45
+    return ('El precio de venta de esta peli es de $' + result)
+  }
+  //DEJO LA FUNCION DE ALQUILAR PARA COLOCARLA LUEGO COMO UNA ACCION OPCIONAL Y MÁS DIRECTA PARA EL USUARIO
+  youWant(movie){
+    let a = confirm(`¿Querés alquilar "${movie.title}"?`)
+    if (a == true) {
+      let dias =Number( prompt('¿Cuántos días?'));
+        if (dias != 0) {
+             return ('A pagar $'+ dias * (movie.fullprice * 0.02) + ' .Disfruta la película!')
+         }else{
+          return console.log('Para otro momento entones :)')
+      }
+    }else{
+      return console.log('Para otro momento entones :)')
+    }
+  }
+
+  showprice(movie){
+    let result= movie.fullprice * 0.45
+    return ('El precio de venta de esta peli es de $' + result)
+  }
+  //lA OPCION DE COMPRA FUNCIONARÁ PARA LA ELECCION DEL USUARIO Y PARA EL OFRECIMIENTO COMERCIAL
   buy(movie){
     let cart = [];
     let quiere = confirm(`¿Quieres comprar ${movie.title}?`)
@@ -43,6 +72,17 @@ class Person{
       }
     }
 }
+//DEFINO PELICULAS DISPONIBLES:
+const MovieOne = new Movie('El señor de los anillos', 'Fantasía,Aventura', '3:21 horas', 2003, 3000)
+const MovieTwo = new Movie('El castillo en el cielo','Fantasía, Aventura','2:04 horas', 1989, 2800)
+const MovieThree = new Movie('Actividad Paranormal', 'Terror', '1:20 horas', 2007, 2500)
+const MovieFour = new Movie('Pulp Fiction', 'thriller, comedia negra','2:58horas', 1994, 2500)
+const MovieFive = new Movie('La lista de Schindler','drama','3:17 horas', 1993, 3000)
+const MovieSix = new Movie('El club de la pelea','drama, comedia', '2:31 horas', 1999, 2500)
+const MovieSeven = new Movie('Forrest Gump', 'drama,romance', '2:22 horas', 1994, 2300)
+const MovieEight = new Movie('Inglourious Basterds','bélico, accion', '2:33 horas', 2009, 3000)
+const MovieNine = new Movie('El padrino','crime,drama', '2:58 horas', 1972, 3000)
+const MovieTen = new Movie('Origen', 'ciencia ficcion', '2:42 horas', 2010,2800)
 
 //DEFINO UN ARRAY DE PERSONAS O USUARIOS:
 const community = [];
@@ -68,14 +108,13 @@ function logIn() {
     return'Usuario sin registrar'
   }
 }
-const MovieOne = new Movie('El señor de los anillos', 'fantasía,aventura', '3:21 horas', 2003, 3000)
 console.log(logIn()); 
 console.log(community);
-//PRUEBO COMO SE VAN AGREGANDO LOS USUARIOS
+
 console.log(logIn()); 
-console.log(community)
-//Pruebo función
+
 console.log(community[0].buy(MovieOne));
+console.log(community[1].buy(MovieOne));
 
 
 
